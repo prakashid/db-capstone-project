0f976c36d464603e843b081112096acaf2573dfa
prakashid <65860478+prakashid@users.noreply.github.com>
2024-02-26 18:52:23 +0530

    Update Prepared_Statement.sql

--- Prepared_Statement.sql
+++ Prepared_Statement.sql
@@ -1,3 +1,14 @@
 PREPARE GetOrderStatement from 'select BookingID, Quantity, BillAmount from Orders where OrderID=?';
 SET @Order_id=4;
 Execute GetOrderStatement USING @Order_id;
+
+PREPARE GetOrderDetails from 'Select OrderID, TableNo, MenuID, BookingID, Quantity, BillAmount from Orders where OrderID=?';
+SET @ID=3;
+Execute GetOrderDetails USING @ID;
+
+PREPARE GetMaxOrder from 'Select Max(Quantity) from Orders';
+EXECUTE GetMaxOrder;
+
+PREPARE DeletOrder from 'Delete from Orders where OrderID=?';
+SET @I_D =3;
+EXECUTE DeletOrder USING @I_D;
